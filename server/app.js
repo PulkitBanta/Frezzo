951b949a5a01c32efe11042ac662e398342e497b
abhishekashyap <29458374+abhishekashyap@users.noreply.github.com>
2020-02-25 23:50:30 +0530

    Initialized socket connection

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Server initialized')
-})
+server.listen(PORT, console.log(`Server is listening on ${PORT}`));
 
-app.listen(PORT, () => console.log(`App is listening on ${PORT}`))
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
