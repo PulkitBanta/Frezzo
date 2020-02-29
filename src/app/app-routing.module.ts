92e241d10b8eec0bc99df04b7169a4fcbe1e28f3
Pulkit <Pulkitbanta55555@gmail.com>
2020-02-29 09:58:24 +0530

    Fixed Components rendering

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, Router } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component'
+import { SellerComponent } from './seller/seller.component';
+import { BuyerComponent } from './buyer/buyer.component';
 
 const routes: Routes = [
-    { path: '', component: MainMenuComponent }
+    { path: '', component: BuyerComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { InputComponent } from './input-field/test.component';
 import { MenuCardComponent } from './menu-card/menu-card.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { TopPickMenuComponent } from './top-pick-menu/top-pick-menu.component';
-import { OptionsComponent } from './options/options.component'
+import { OptionsComponent } from './options/options.component';
+import { BuyerComponent } from './buyer/buyer.component';
+import { SellerComponent } from './seller/seller.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    BuyerComponent,
     InputComponent,
-    MenuCardComponent,
     MainMenuComponent,
+    MenuCardComponent,
+    OptionsComponent,
     TopPickMenuComponent,
-    OptionsComponent
+    SellerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
