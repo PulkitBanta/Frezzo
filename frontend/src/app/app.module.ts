9c3d1a178e085a8ad2a1e3377abac6711ce06b55
Pulkit <Pulkitbanta55555@gmail.com>
2020-06-19 11:26:41 +0530

    removed components and service

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AsyncPipe } from '@angular/common';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    AsyncPipe
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
